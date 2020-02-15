174fc0461509b463c8f2b99e9ab6b0fafe3d2579
ropraname <45720319+ropraname@users.noreply.github.com>
2020-02-15 11:17:41 +0300

    Update new_steppers_forRBI.ino

--- new_steppers_forRBI.ino
+++ new_steppers_forRBI.ino
@@ -1,9 +1,9 @@
 #include<AccelStepper.h>
 
-#define motorPin1  3 // IN1 на 1-м драйвере ULN2003
-#define motorPin2  6 // IN2 на 1-м драйвере ULN2003
-#define motorPin3  2 // IN3 на 1-м драйвере ULN2003
-#define motorPin4  5 // IN4 на 1-м драйвере ULN2003
+#define motorPin1  3
+#define motorPin2  6
+#define motorPin3  2
+#define motorPin4  5
 
 AccelStepper stepper_x(1, 3, 6);//красный (2 слева)
 AccelStepper stepper_y(1, 2, 5);
